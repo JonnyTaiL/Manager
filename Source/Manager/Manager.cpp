f71abdda4e52f66ee4f23e9fbc8de556f0390611
JonnyTaiL <Ivan.imiridi@mail.ru>
2025-01-31 13:08:03 +0300

    InitialCommit

---
+++ Source/Manager/Manager.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Manager.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Manager, "Manager" );
