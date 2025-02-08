07b30d6e9cd92122148352a4e6a657500c10ea6d
Max <m.apasov2014@yandex.ru>
2025-02-08 15:58:22 +0300

    WiP_RegistrationAuthorization_01

--- Source/Manager/W_PandaOSMain.h
+++ Source/Manager/W_PandaOSMain.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/EditableTextBox.h"
+#include "HTTPModule.h"
+#include "Interfaces/IHttpResponse.h"
 #include "W_PandaOSMain.generated.h"
 
 /**
@@ -13,5 +16,18 @@ UCLASS()
 class MANAGER_API UW_PandaOSMain : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeConstruct() override;
+
+	UFUNCTION(BlueprintCallable, Category = "User\|Authorization")
+	void UserAuthorize(FString Login, FString Password);
+	void UserAuthorizeAnswerReceive(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	UFUNCTION(BlueprintCallable, Category = "User\|Registration")
+	void UserRegister(FString Login, FString Password, FString Name, FString Surname, FString Patronymic, int32 Group, int32 Permission);
+	void UserRegisterAnswerReceive(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	TSubclassOf<UEditableTextBox> TB_Information;
 };
