359cd0747d4ebe0c1406f521dfa8984b60d7266a
JonnyTaiL <Ivan.imiridi@mail.ru>
2025-02-15 12:14:44 +0300

    WiP_001e
    -OSButtonPopUp added to c++

---
+++ Source/Manager/W_OSButtonOptions.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "W_OSButtonOptions.h"
+#include "Components/TextBlock.h"
+#include "W_Button_Default.h"
+
+
+void UW_OSButtonOptions::NativeConstruct()
+{
+
+}
